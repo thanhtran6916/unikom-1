aecda770dcc8340ccf8c956cd028c0bf92c3c05a
thanhtran <tranvanthanhtran.tt@gmail.com>
2021-12-28 12:07:21 +0700

    create AuthRestController

---
+++ src/main/java/com/example/demo/controller/AuthRestController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.JwtResponse;
+import com.example.demo.model.User;
+import com.example.demo.service.jwt.JwtService;
+import com.example.demo.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin("*")
+@RequestMapping("/auth")
+public class AuthRestController {
+
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("login")
+    public ResponseEntity<JwtResponse> login(@RequestBody User user) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        String token = jwtService.createTokenLogin(authentication);
+        return new ResponseEntity<>(new JwtResponse(token), HttpStatus.OK);
+    }
+
+}
